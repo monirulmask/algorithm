be74ef589610ccbfde163bd9ec456b3c24447aa5
Jahirul Islam <rajumjib@aol.com>
2018-08-11 17:40:47 +0600

    First Commit

---
+++ src/main/java/com/online4help/algorithm/AiAlgorithmApplication.java
@@ -0,0 +1,12 @@
+package com.online4help.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAlgorithmApplication.class, args);
+	}
+}
